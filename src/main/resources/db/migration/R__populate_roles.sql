a1d012c609db2ccb613a609bb9341c3950011b82
Alexander Zotz <alexander.zotz.external@eon.de>
2023-10-14 07:46:33 +0200

    initialize database for local development.

---
+++ src/main/resources/db/migration/R__populate_roles.sql
@@ -0,0 +1,1 @@
+INSERT INTO t_role (id, name) VALUES (gen_random_uuid(), 'ROLE_USER');
---
+++ src/main/resources/db/migration/V002__create_tables.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS t_dog
+(
+    id    uuid    NOT NULL,
+    name  varchar NOT NULL,
+    age   integer NOT NULL,
+    breed varchar NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS t_dog_image
+(
+    id     uuid    NOT NULL,
+    dog_id uuid    NOT NULL,
+    url    varchar NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (dog_id) REFERENCES t_dog (id)
+);
+
+CREATE TABLE IF NOT EXISTS t_app_user
+(
+    id       uuid    NOT NULL,
+    username varchar NOT NULL,
+    password varchar NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS t_role
+(
+    id   uuid    NOT NULL,
+    name varchar NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS t_user_roles
+(
+    id      uuid NOT NULL,
+    user_id uuid,
+    role_id uuid,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES t_app_user (id),
+    FOREIGN KEY (role_id) REFERENCES t_role (id)
+);
