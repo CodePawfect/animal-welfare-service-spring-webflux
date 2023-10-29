ba2b1384bc6752bb8f8861ae038a9159de4e5de0
Alexander Zotz <alexander.zotz.external@eon.de>
2023-10-29 20:39:51 +0100

    prepare azure blob storage integration with azurite testcontainer.
    add azure blob storage dependency and BlobStorageService.

---
+++ src/main/java/com/github/codepawfect/animalwelfareservicespringboot/core/service/BlobStorageService.java
@@ -0,0 +1,57 @@
+package com.github.codepawfect.animalwelfareservicespringboot.core.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import com.azure.storage.blob.BlobServiceClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlobStorageService {
+
+  private final BlobServiceClient blobServiceClient;
+
+  public BlobStorageService(BlobServiceClient blobServiceClient) {
+    this.blobServiceClient = blobServiceClient;
+  }
+
+  /**
+   * Upload files to blob storage
+   * @param containerName container name
+   * @param blobName blob name
+   * @param data data
+   */
+  public void uploadToBlob(String containerName, String blobName, InputStream data) throws IOException {
+    var blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
+    var blobClient = blobContainerClient.getBlobClient(blobName);
+    blobClient.upload(data, data.available(), true);
+  }
+
+  /**
+   * Read from blob storage
+   * @param containerName container name
+   * @param blobName blob name
+   * @return data
+   */
+  public byte[] readFromBlob(String containerName, String blobName) throws IOException {
+    var blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
+    var blobClient = blobContainerClient.getBlobClient(blobName);
+
+    try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+      blobClient.downloadStream(outputStream);
+      return outputStream.toByteArray();
+    }
+  }
+
+  /**
+   * Delete from blob storage
+   * @param containerName container name
+   * @param blobName blob name
+   */
+  public void deleteFromBlob(String containerName, String blobName) {
+    var blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
+    var blobClient = blobContainerClient.getBlobClient(blobName);
+    blobClient.delete();
+  }
+
+}
